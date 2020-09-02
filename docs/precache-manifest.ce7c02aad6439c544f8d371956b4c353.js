9b1d7114a4a59477bd593dbc41e90728c3948d43
Alejandro <alz@aviatel.com>
2020-09-02 19:44:38 -0300

    docs

---
+++ docs/precache-manifest.ce7c02aad6439c544f8d371956b4c353.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5f5c63306fdfc78e70b641a8f8343f13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c93782a8c07cd931c290",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "76b6b4257746597d704c",
+    "url": "/static/js/2.8eb8d0cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8eb8d0cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c93782a8c07cd931c290",
+    "url": "/static/js/main.9b6aa57d.chunk.js"
+  },
+  {
+    "revision": "2c3870da4bf6593bc417",
+    "url": "/static/js/runtime-main.f5e01df6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
